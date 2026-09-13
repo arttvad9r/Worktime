a0d12fdbe6b295d1f0d42954173c4a7ff4fde70d
artt <arttvad9r@gmail.com>
2026-09-13 03:54:29 +0300

    test: fix Compose v2 test API imports

--- app/src/androidTest/java/com/worktime/app/modern/ModernWorkTimeFlowTest.kt
+++ app/src/androidTest/java/com/worktime/app/modern/ModernWorkTimeFlowTest.kt
@@ -6,8 +6,6 @@ import androidx.compose.ui.test.hasContentDescription
 import androidx.compose.ui.test.hasSetTextAction
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.v2.createAndroidComposeRule
-import androidx.compose.ui.test.onAllNodes
-import androidx.compose.ui.test.onNode
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextReplacement
